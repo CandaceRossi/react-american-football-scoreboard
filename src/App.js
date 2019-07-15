c8f68408aff10e149ef2c8b689130317c677f688
CandaceRossi <50240571+CandaceRossi@users.noreply.github.com>
2019-07-15 18:11:36 -0400

    removed some of the extra code I was overthinking. working on buttons to work now

--- src/App.js
+++ src/App.js
@@ -6,67 +6,52 @@ import BottomRow from "./BottomRow";
 
 function App() {
     //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
-    const [LionScore, tigerScoreState] = useState(0);
-    console.log("homeScore", homeScore);
-    const [TigerScore, lionScoreState] = useState(0);
-    console.log("awayScore", awayScore)
-    madeTouchDown[sevenPoints, addSevenPoints] = useState(7);
-    madeFieldGoal[threePoints, addThreePoints] = useState(3);
-    const scoreBoard() => {
-        madeTouchDown === true ? addSevenPoints() : madeFieldGoal === true ? addThreePoints() : +0;
-    }
+    const [LionScore, lionScoreState] = useState(0);
+    console.log("homeScore", LionScore);
+    const [TigerScore, tigerScoreState] = useState(0);
+    console.log("awayScore", TigerScore);
 
-}
-const buttons() {
-        homeScoreState({
-            this.Score = { homeScore + {} }
-        })
-    }
-    // this.state = { 
-    //   'home score': 0; 
-    // }
-    //away score state
 
-return ( <
-    div className = "container" >
-    <
-    section className = "scoreboard" >
-    <
-    div className = "topRow" >
-    <
-    div className = "home" >
-    <
-    h2 className = "home__name" > Lions < /h2>
+    return ( <
+        div className = "container" >
+        <
+        section className = "scoreboard" >
+        <
+        div className = "topRow" >
+        <
+        div className = "home" >
+        <
+        h2 className = "home__name" > Lions < /h2>
 
-    { /* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */ }
+        { /* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */ }
 
-    <
-    div className = "home__score" > 32 < /div> < /
-    div > <
-    div className = "timer" > 00: 03 < /div> <
-    div className = "away" >
-    <
-    h2 className = "away__name" > Tigers < /h2> <
-    div className = "away__score" > 32 < /div> < /
-    div > <
-    /div> <
-    BottomRow / >
-    <
-    /section> <
-    section className = "buttons" >
-    <
-    div className = "homeButtons" > { /* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */ } <
-    button className = "homeButtons__touchdown" > Home Touchdown < /button> <
-    button className = "homeButtons__fieldGoal" > Home Field Goal < /button> < /
-    div > <
-    div className = "awayButtons" >
-    <
-    button className = "awayButtons__touchdown" > Away Touchdown < /button> <
-    button className = "awayButtons__fieldGoal" > Away Field Goal < /button> < /
-    div > <
-    /section> < /
-    div >
-);
+        <
+        div className = "home__score" > { LionScore } < /div> < /
+        div > <
+        div className = "timer" > 00: 03 < /div> <
+        div className = "away" >
+        <
+        h2 className = "away__name" > Tigers < /h2> <
+        div className = "away__score" > { TigerScore } < /div> < /
+        div > <
+        /div> <
+        BottomRow / >
+        <
+        /section> <
+        section className = "buttons" >
+        <
+        div className = "homeButtons" > { /* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */ } <
+        button className = "homeButtons__touchdown" > Home Touchdown < /button> <
+        button className = "homeButtons__fieldGoal" > Home Field Goal < /button> < /
+        div > <
+        div className = "awayButtons" >
+        <
+        button className = "awayButtons__touchdown" > Away Touchdown < /button> <
+        button className = "awayButtons__fieldGoal" > Away Field Goal < /button> < /
+        div > <
+        /section> < /
+        div >
+    );
 }
 
 export default App;
