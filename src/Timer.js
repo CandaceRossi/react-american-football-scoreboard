6c8db9b3b33d927cbe304a44f238e9eb304ddfb2
CandaceRossi <50240571+CandaceRossi@users.noreply.github.com>
2019-08-12 17:30:26 -0400

    made useState hooks for teams, attached dynamic state to score and made onClick logic for Touchdown Fieldgoal

--- src/Timer.js
+++ src/Timer.js
@@ -1,18 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import "./App.css";
 
-
 const Timer = () => {
-    const [time, setTime] = useState(90);
-    const startTimer = () => {
-        setInterval(() => setTime(time - 1), 60000)
-    }
-    return <div >
-        <
-        div className = "timer" > 00: 03 < /div> <
-        button onClick = {
-            () => startTimer() } > Start TImer < /button> <
-        /div>
-}
+  const [time, setTime] = useState(90);
+  const startTimer = () => {
+    setInterval(() => setTime(time - 1), 60000);
+  };
+  return (
+    <div>
+      <div className="timer"> 00: 03 </div>
+
+      <button onClick={() => startTimer()}> Start TImer </button>
+    </div>
+  );
+};
 
 export default Timer;
